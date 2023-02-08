f1368450a00203889ac2e180153303444ecb593d
abubakkariiui <abubakkarmit@gmail.com>
2023-02-08 15:09:31 +0500

    update

--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -3,7 +3,8 @@ import { TodoItem } from './types';
 const dummyData: TodoItem[] = [
   { id: '1', todo: 'Eat', complete: true },
   { id: '2', todo: 'Sleep', complete: false },
-  { id: '3', todo: 'Repeat', complete: false },
+  { id: '3', todo: 'Code', complete: false },
+  { id: '4', todo: 'Repeat', complete: false },
 ]
 
 export default dummyData;
